7b4b40441ea754aec4f07ea3b69cbea580ba550f
ManuelLJackson25 <80664455+ManuelLJackson25@users.noreply.github.com>
2021-05-03 08:40:16 +0200

    Klasse Eigentümer

---
+++ Eigentuemer.java
@@ -0,0 +1,15 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Eigentuemer.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Eigentuemer
+{
+    public Eigentuemer()
+    {
+    
+    }
+    
+}
